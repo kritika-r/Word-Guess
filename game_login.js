0a1e37133158416f9eb2ed562ad955d1f3afa4f1
kritika-r <82035781+kritika-r@users.noreply.github.com>
2021-06-03 17:56:06 +0530

    fully-functional-word-guess-game

--- game_login.js
+++ game_login.js
@@ -2,9 +2,12 @@ var p1 = localStorage.getItem("Name_of_Player1");
 var p2 = localStorage.getItem("Name_of_Player2");
 console.log("The player1 is " +p1);
 console.log("The player2 is " +p2);
+var word_1_caps= "";
+var word_1= "";
 
 var p1_score = 0;
 var p2_score = 0;
+console.log("The player1 score is " +p1_score+ "The player2 score is " +p2_score);
 
 document.getElementById("player1_name").innerHTML = p1 + ": ";
 document.getElementById("player2_name").innerHTML = p2 + ": ";
@@ -31,13 +34,66 @@ function send(){
      var r3= r2.replace(c3,"_ ");
 
     var first_word="<h4 id='word_display'> Q. " +r3+"</h4>";
-    var input_box="<br>Answer:  <input type='text' placeholder='Enter your answer here'>";
+    var input_box="<br>Answer:  <input id='input_answer' type='text' placeholder='Enter your answer here'>";
     var check_button = "<br> <button id='check_button' class='btn btn-primary'onclick='check()'>Check</button>";
     var row= first_word + input_box + check_button;
 
     document.getElementById("output").innerHTML = row;
+     word_1_caps= document.getElementById("question_input").value;
+     word_1= word_1_caps.toLowerCase();
+    console.log(word_1);
     document.getElementById("question_input").value = "";
     
 }
+
+var question_turn= "Player1";
+var answer_turn= "Player2";
+
+function check(){
+    
+    console.log(word_1);
+    var get_answer =document.getElementById("input_answer").value;
+    var answer= get_answer.toLowerCase();
+    console.log("The answer given is: " +answer);
+    if(answer == word_1 ){
+        console.log("The player1 score is " +p1_score+ "The player2 score is " +p2_score);
+
+        if(answer_turn == "Player1"){
+            p1_score = p1_score + 1;
+            console.log("The player1 score is " +p1_score+ "The player2 score is " +p2_score);
+
+            document.getElementById("player1_score").innerHTML = p1_score;
+
+        }
+        else{
+            p2_score = p2_score + 1;
+            console.log("The player1 score is" +p1_score+ "The player2 score is " +p2_score);
+
+            document.getElementById("player2_score").innerHTML = p2_score;
+        }
+    }
+    if(question_turn == "Player1"){
+        question_turn= "Player2";
+        document.getElementById("player_question").innerHTML = "Question Turn: " +p2;
+
+    }
+    else{
+        question_turn= "Player1";
+        document.getElementById("player_question").innerHTML = "Question Turn: " +p1;
+    }
+    if(answer_turn == "Player1"){
+        answer_turn= "Player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn: " +p2;
+
+    }
+    else{
+        answer_turn= "Player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn: " +p1;
+    }
+    
+    document.getElementById("output").innerHTML= "";
+    
+}
+
 
 
